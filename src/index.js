fec75ad1d2372ef5e1faaf7394fca2ae2e262179
Eskov Dmitriy <dmteskov@gmail.com>
2022-08-12 20:15:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  console.log(brackets)
+  let stack = [];
+  for(let i = 0; i<str.length; i++){
+    let bracketsIndex = brackets.indexOf(str[i]);
+    if(bracketsIndex % 2 === 0){
+      if(str[i] === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+        stack.pop();
+      }else if (str[i] === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+        stack.push(bracketsIndex)
+      }
+      else{
+        stack.push(bracketsIndex)
+      }
+    } 
+    else {
+    if (stack.pop() !== bracketsIndex-1){
+        return false;
+    }
 }
+}return stack.length === 0
+// console.log(stack.length)
+};
